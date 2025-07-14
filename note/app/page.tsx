31012a282335f3dbbbc8f056749495bd1ed7cb68
mohamoha0000 <mohamedelmaeyouf@gmail.com>
2025-07-14 13:41:53 +0100

    v4

--- note/app/page.tsx
+++ note/app/page.tsx
@@ -1,7 +1,46 @@
-
+import Image from 'next/image';
 
 export default function HOME(){
   return(
-    <h1>hhh</h1>
+    <div>
+       <header style={{ backgroundColor: '#DCD0A8'}} className="flex items-center justify-between p-4">
+      <h1>Notes</h1>
+      <ul className="flex gap-x-4">
+        <li className="cursor-pointer">
+          <a className="pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300">
+            Home
+          </a>
+        </li>
+        <li className="cursor-pointer">
+          <a className="pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300">
+            About
+          </a>
+        </li>
+        <li className="cursor-pointer">
+          <a className="pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <button style={{ backgroundColor: '#4A9782'}} className="text-white p-2 rounded-md">Get Started</button>
+    </header>
+    <main>
+      <div className='flex items-center justify-around m-2'>
+        <div className='w-1/3'>
+          <Image
+          src="/images/ding.png" // or from public folder
+          alt="Logo"
+          width={400}
+          height={400}
+          className=""
+        />
+        </div>
+        <div className='w-1/3'>
+          <h1 className='text-3xl text-notes-green'>Notes organize</h1>
+          <p>Quickly capture, organize, and access your notes anytime, anywhere. Our platform makes it easy to jot down ideas, manage tasks, and stay productive—all in one place.</p>
+        </div>
+      </div>
+    </main>
+    </div>
   )
 }
